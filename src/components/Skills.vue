097578ff37870644c5b206804c921ff927f1de4a
abedinevis <mbapost@yahoo.com>
2018-11-24 15:12:33 +0330

    lists

---
+++ src/components/Skills.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    {{ name }}
+
+    {{ btnState ? 'the buttun is disabled' : 'the bu'}}
+  <button @click="changeName"
+          :disabled="btnState">
+  Change Name
+  </button>
+  <div class="holder">
+    <ul>
+      <li v-for="(data,index) in skills" :key='index'>
+        {{ index+1 }}. {{ data.skill }}
+      </li>
+    </ul>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      name: 'MohammadBagher',
+      btnState: true,
+      skills: [
+        { "skill": "vue-js"},
+        {"skill": "front end developer"},
+        { "skill": "vue-js"},
+
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
