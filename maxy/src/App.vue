74d5dac942f987524e473029d819b24254411cab
abedinevis <mbapost@yahoo.com>
2018-12-17 19:39:06 +0330

    config toolbaritems  with v-for

--- maxy/src/App.vue
+++ maxy/src/App.vue
@@ -2,24 +2,30 @@
   <v-app>
     <v-navigation-drawer absolute temporary v-model="sideNav">
       <v-list>
-        <v-list-tile>
+
+        <v-list-tile v-for="item in menuItems"  :key="item.title">
           <v-list-tile-action>
-            <v-icon>supervisor_account</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>view meetups</v-list-tile-content>
+
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
         </v-list-tile>
+
       </v-list>
     </v-navigation-drawer>
+
     <v-toolbar dark class="primary">
       <v-toolbar-title>DevMeetup</v-toolbar-title>
       <v-toolbar-side-icon @click.stop="sideNav = !sideNav"></v-toolbar-side-icon>
       <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
+
+      <v-toolbar-items class="hidden-xs-only" v-for="item in menuItems" :key="item.title">
         <v-btn flat>
-        <v-icon left>supervisor_account</v-icon>  
-          view meetups
+        <v-icon left>{{ item.icon }}</v-icon>  
+          {{ item.title }}
         </v-btn>
       </v-toolbar-items>
+
     </v-toolbar>
     <main>
 
@@ -31,7 +37,14 @@
 export default {
   data () {
     return {
-      sideNav: false
+      sideNav: false,
+      menuItems: [
+          {icon: 'supervisor_account', title: 'View Meetups'},
+          {icon: 'room', title: 'Organize Meetups'},
+          {icon: 'person', title: 'Profile'},
+          {icon: 'face', title: 'Sign up'},
+          {icon: 'lock', title: 'Sign in'}
+      ]
     }
   },
   name: 'App'
